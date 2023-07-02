1a3561c058f322f0937d0cad4bb55f07497a54eb
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2023-07-02 15:50:29 +0200

    Disable extern database functionality

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///tasks.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
